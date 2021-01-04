20bdbb93400fc0087fd75a277e9160a7c078ee94
thehalfbloodprince00006@outlook.com <thehalfbloodprince00006@outlook.com>
2021-01-04 21:32:31 +0600

    perlin noise

---
+++ Assets/PerlinNoise/_Scripts/CubeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+
+    public static float div = 20f;
+
+    private float maxOffset = 5.5f;
+
+    void Update()
+    {
+        float perlin = Mathf.PerlinNoise(transform.position.x / CubeScript.div + Time.time, transform.position.z / CubeScript.div + Time.time);
+        transform.localScale = new Vector3(1f, perlin * maxOffset, 1f);
+    }
+}
